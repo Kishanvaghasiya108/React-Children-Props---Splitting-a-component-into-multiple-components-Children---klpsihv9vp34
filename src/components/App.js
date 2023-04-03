457a891d03372a30f34099680803b38d339a628c
Kishanvaghasiya108 <kishanvaghasiya2008@gmail.com>
2023-04-03 18:07:49 +0530

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
+ const [square,setSquare] = useState(0);
 
+ const changeHandler = (e) => {
+  // console.log(e.target)
+  setSquare(Number(e.target.value)*Number(e.target.value))
+
+
+ }
   return (
     <div id="main">
       <ChildComponent /> 
-       <input id='input' onChange={} />
+       <input id='input' onChange={changeHandler} />
     
-      <p id='output'>{}</p>
+      <p id='output'>{square}</p>
     </div>
   )
 }
